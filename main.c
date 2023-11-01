8eea43524a105e475fb72453e3b97fd8ad2141a5
Darkinki <bondarenkoas2004@gmail.com>
2023-11-01 18:59:15 +0200

    attempt in pokker test

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include "monoBite.h"
 #include "maxLenghtTest.h"
+#include <math.h>
 
 #define capacity 20000
 
@@ -21,6 +22,26 @@ int main() {
     printf("counter of series is %d \n", counter);
 
     seriesTest(counter, max_series);
+//
+
+    int Y = 100;
+    int k = 5;
+    int m = 4;
+    int ni[5] = {10, 15, 20, 25, 30};
+    double X3 = 0;
+    double sum_ni_squared = 0;
+
+    for (int i = 0; i < k; i++) {
+        sum_ni_squared += pow(ni[i], 2);
+    }
+
+    X3 = (2 * m / k) * (sum_ni_squared - k);
+
+    if (X3 > 1.03 && X3 < 57.4) {
+        printf("Послiдовнiсть вважається випадковою.\n");
+    } else {
+        printf("Послiдовнiсть не випадкова.\n");
+    }
 
     return 0;
 }
